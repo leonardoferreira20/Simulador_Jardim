43443a42c87a9096bd5bdad4c3dc48d663dd183c
Hoorror555 <joaorama.eng@gmail.com>
2025-10-29 21:41:32 +0000

    Está Testado o comando espero que nao dê mais erros

--- project/Comando/Comando.cpp
+++ project/Comando/Comando.cpp
@@ -38,7 +38,6 @@ void Comando::comandoAvanca(Jardim* jardim, istringstream& iss){
     iss >> n;
     jardim->avanca(n);
     jardim->imprimir();
-    cout << "Isto é para o Joao remover" << endl;
 };
 
 void Comando::comandoPlantar(Jardim* jardim, istringstream& iss){
